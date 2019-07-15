133f02acde1a1c134b5d1402c0051685cbde7486
OminousFlake <rus48goldfinch@gmail.com>
2019-07-15 15:28:42 +0300

    Work in progress

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -6,9 +6,8 @@ import ConversationsContainer from "./Conversations/ConversationsContainer";
 const Messages = () => {
     return (
         <div className={s.messages}>
-            <h1>В разработке! Да, даже отступ слева не добавлю!</h1>
-                <ConversationsContainer/>
-                <DialoguesContainer/>
+            <ConversationsContainer/>
+            <DialoguesContainer/>
         </div>
     )
 };
