da81d93c6dd7b01bac8ba091e9d1d0105785b59e
OminousFlake <rus48goldfinch@gmail.com>
2019-10-25 13:05:58 +0300

    Исправил пути для гита

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import store from './redux/redux-store';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
 ReactDOM.render(
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter>
             <Provider store={store}>
             <App/>
             </Provider>
-        </BrowserRouter>, document.getElementById('root'));
+        </HashRouter>, document.getElementById('root'));
 
